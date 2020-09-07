8ffc11a48021a8593d2b1858fa5fbe58ce1e952a
nvcTuan2302 <nvctuan2302@gmail.com>
2020-09-08 00:41:17 +0700

    fix html

--- src/js/weather.js
+++ src/js/weather.js
@@ -13,38 +13,40 @@ let now = new Date()
 
 
 const success = position => {
-  const {latitude, longitude} = position.coords
+  const { latitude, longitude } = position.coords
 
   fetch(`${api.baseUrl}weather?lat=${latitude}&lon=${longitude}&units=metric&appid=${api.key}`)
-  .then(respons => respons.json())
-  .then(displayWeather)
-  .catch(error)
+    .then(respons => respons.json())
+    .then(displayWeather)
+    .catch(error)
 }
 
 const getResults = query => {
   fetch(`${api.baseUrl}weather?q=${query}&units=metric&appid=${api.key}`)
-  .then(respons => respons.json())
-  .then(displayWeather)
-  .catch(error)
+    .then(respons => respons.json())
+    .then(displayWeather)
+    .catch(error)
 }
 
-const displayWeather =  data => {
-  const {temp_min, temp_max, temp} = data.main
+const displayWeather = data => {
+  const { temp_min, temp_max, temp } = data.main
 
   nameCity.innerText = `${data.name} ,${data.sys.country}`
   date.innerText = now.toDateString()
-  icon.innerHTML = `<div class="icon js-icon" style="background-image: url('http://openweathermap.org/img/wn/${data.weather[0].icon}@4x.png');"></div>`
-//   tempMin.innerText = temp_min
-  tempMid.innerHTML = data.main.temp
-//   tempMax.innerText = temp_max
+  icon.innerHTML =
+   `<div class="icon js-icon" style="background-image: url('http://openweathermap.org/img/wn/${data.weather[0].icon}@4x.png');"></div>`
+  //   tempMin.innerText = temp_min
+  tempMid.innerHTML = temp
+  //   tempMax.innerText = temp_max
   console.log(data)
 }
 
 const error = err => console.warn(`ERROR(${err.code}): ${err.message}`);
 
 export const weather = _=> {
-      navigator.geolocation.getCurrentPosition(success)
-  
+  document.body.style.backgroundImage = `url('https://images.pexels.com/photos/158163/clouds-cloudporn-weather-lookup-158163.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260')`
+  navigator.geolocation.getCurrentPosition(success)
+
   searchBox.addEventListener('keypress', e => {
     if (e.keyCode === 13) {
       getResults(e.target.value)
